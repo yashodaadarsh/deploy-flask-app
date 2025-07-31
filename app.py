41567f344c786931fb9e06894f062099b5840ce1
= <=>
2025-07-31 14:19:20 +0530

    modified

--- app.py
+++ app.py
@@ -5,4 +5,9 @@ app = Flask(__name__)
 def home():
     return "Hello from Render!"
 
+@app.route('/about')
+def about():
+    return "This is the about page."
+# The app is ready to be deployed on Render
+# Gunicorn will be used as the WSGI server in production
 # Don't need app.run() when using gunicorn
